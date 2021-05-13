22482eb18a6fd60af7469ff82f7b9520bde359b8
Argenis Osorio <argenisosorio580@hotmail.com>
2021-05-13 12:12:48 -0400

    Added v-for example

--- app.js
+++ app.js
@@ -9,5 +9,12 @@ Vue.component('movie-card', {
 })
 
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    movies: [
+      { title: 'Regreso al Futuro', image: 'http://es.web.img3.acsta.net/pictures/14/04/03/13/45/034916.jpg' },
+      { title: 'Matrix', image: 'http://t0.gstatic.com/images?q=tbn:ANd9GcQq3pIz-aKgkmYX1dJ-EL-AlHSPcOO7wdqRIJ5gJy9qNinXpmle' },
+      { title: 'Interestellar', image: 'http://t1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB' }
+    ]
+  }
 })
